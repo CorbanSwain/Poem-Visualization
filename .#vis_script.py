fcc7579bbd34943004197757d9bc80f0fec8cc64
CorbanSwain <CorbanSwain@gmail.com>
2018-03-18 22:53:37 -0400

    working wavefile converter

--- .#vis_script.py
+++
@@ -1,1 +0,0 @@
-CorbanSwain@UNDERSTANDING.local.39487
--- vis_script.py
+++ vis_script.py
@@ -13,32 +13,16 @@ clipname = 'test2'
 clipname += '.wav'
 clipfile = os.path.join(clipdir, clipname) 
 
-audio = wave.open(clipfile, 'r')
+def wav_to_float(wavefile):
+    audio = wave.open(wavefile, 'r')
+    output = []
+    for iFrame in range(audio.getnframes()):
+        wave_data = audio.readframes(1)
+        data = struct.unpack('<i', wave_data)
+        output.append(data[0])
+    max_val = max(output)
+    output = [v / max_val for v in output]
+    audio.close()
+    return output
 
-print('N Channels: %d' % audio.getnchannels())
-print('N Frames: %d' % audio.getnframes())
-print('Sample Width: %d bytes' % audio.getsampwidth())
-
-nframes_to_read = 50
-for iFrame in range(nframes_to_read):
-    wave_data = audio.readframes(1)
-    data = struct.unpack('<i', wave_data)
-    print('%2d - %10d' % (iFrame, int(data[0])))
-
-
-
-# def wav_to_floats(w):
-#     astr = w.readframes(w.getnframes())
-#     print('Length of output = %d' % len(astr))
-#     print('length out / (nfames * nchanels) = %d' % (len(astr) / (w.getnframes() * w.getnchannels())))
-#     # convert binary chunks to short 
-#     a = struct.unpack("%iH" % (w.getnframes()* w.getnchannels()), astr)
-#     a = [float(val) / pow(2, 15) for val in a]
-#     return a
-
-# # read the wav file specified as first command line arg
-# signal = wav_to_floats(audio)
-# print("read "+str(len(signal))+" frames")
-# print("in the range "+str(min(signal))+" to "+str(min(signal)))
-    
-audio.close()
+amplitude = wav_to_float(clipfile)
