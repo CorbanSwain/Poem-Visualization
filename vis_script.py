4bc93e52481e3819e6aef3c9744f0f2d0a03d04c
CorbanSwain <CorbanSwain@gmail.com>
2018-03-19 01:17:24 -0400

    fully working version

--- vis_script.py
+++ vis_script.py
@@ -1,16 +1,13 @@
 import numpy as np
 import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-from matplotlib.artist import Artist
-from matplotlib.mlab import dist_point_to_segment
+from matplotlib.patches import Polygon
 import os
 import wave
 import struct
 import sys
 
 clipdir = '/Users/CorbanSwain/Google Drive'
-clipname = 'test2'
+clipname = 'nigger'
 
 clipname += '.wav'
 clipfile = os.path.join(clipdir, clipname) 
@@ -34,51 +31,55 @@ def wav_to_float(wavefile):
     audio.close()
     return (time, amplitude)
 
-
+print('Beginning File Import...')
 t, a = np.array(wav_to_float(clipfile))
+print('Finished file import!')
 
 numel = len(t)
 a_rect = np.multiply(a, a)
-window_size = round(numel * 0.5)
+window_size = round(numel * 0.05)
 smooth_fxn_1 = np.ones((window_size,)) / window_size
 def gaussian(x, mu, sig):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 temp_x = np.linspace(-1, 1, window_size)
-smooth_fxn_2 = gaussian(temp_x, 0, 0.2) + 0.25
+smooth_fxn_2 = gaussian(temp_x, 0, 0.2) + (0.5 / window_size) 
 smooth_fxn = smooth_fxn_2
+
+print('Beginning Convolution step...')
 a_smooth = np.convolve(a_rect, smooth_fxn, mode='same')
+print('Finished Convolution!')
+
 a_smooth = a_smooth / max(a_smooth)
 new_numel = 1000
 space = np.floor(numel / new_numel).astype(np.int64)
-print(space)
-print(type(space))
 new_len = round(new_numel * space)
 new_len = new_len.astype(np.int64)
-print(new_len)
-print(type(new_len))
-# FIXME - use slicing
 a_smooth_sub = a_smooth[np.arange(0, new_len, space)]
 t_sub = t[np.arange(0, new_len, space)]
 
-
-
-theta = np.linspace(0, 2 * np.pi, new_len)
+theta = np.linspace(0, 2 * np.pi, new_numel) - (np.pi / 2)
 r = (0.25 + a_smooth_sub) * 5
-
 xs = np.multiply(r, np.cos(theta))
 ys = np.multiply(r, np.sin(theta))
+poly = Polygon(np.column_stack([xs, ys]), True)
 
-poly = mpl.patches.Polygon(np.column_stack([xs, ys]))
-path = poly.get_path()
+print('Calculations are complete!')
 
-do_plot = False
+import matplotlib.pyplot as plt
+
+do_plot = True
 if do_plot:
+    print('Beginning plot 1...')
     plt.figure(0)
     plt.plot(t, a)
     plt.plot(t_sub, a_smooth_sub)
-    plt.show()
+    print('Finished Plot 1')
     
+    print('Beginning plot 2...')
     plt.figure(1)
     ax = plt.subplot(111, aspect='equal')
     ax.add_patch(poly)
+    ax.set_xlim((min(xs), max(xs)))
+    ax.set_ylim((min(ys), max(ys)))
     plt.show()
+    print('Finished Plot 2.')
